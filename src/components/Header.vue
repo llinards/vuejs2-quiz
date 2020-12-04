e6cba73c517ac8b3a4f077486308f08a4c106f17
Linards <linards@lynyrds.com>
2020-12-04 20:45:47 +0200

    project ready

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><strong>Quiz App</strong></b-nav-item>
+      <b-nav-item disabled
+        >Counter: {{ numCorrect }} / {{ numTotal }}</b-nav-item
+      >
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"],
+};
+</script>
